150562ddc784d7d85896617e94ccb0b72f6e5a38
Matheus Pereira <githubmatheus@outlook.com>
2018-12-03 20:28:24 -0200

    config envirionment

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,3 +1,6 @@
+const config = require('config');
+const morgan = require('morgan');
+const helmet = require('helmet');
 const Joi = require('joi');
 const logger = require('./logger');
 const express = require('express');
@@ -6,6 +9,16 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(helmet());
+
+console.log('Application Name: ' + config.get('name'));
+console.log('Mail Server: ' + config.get('mail.host'));
+console.log('Mail Password: ' + config.get('mail.password'));
+
+if (app.get('env') === 'development'){
+    app.use(morgan('tiny'));
+    console.log('Morgan enabled...');
+}
 
 app.use(logger);
 
